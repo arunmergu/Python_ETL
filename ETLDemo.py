e29b4f3657b733f731a1fed1b2829d5e26aec14b
user.name <sc21acf@herts.ac.uk>
2022-08-15 00:13:48 +0100

    comments added

--- ETLDemo.py
+++ ETLDemo.py
@@ -3,57 +3,56 @@ import requests
 from pymongo import MongoClient
 
 
-def getApiData(api_url):
-    try:
-        # Extracting the data from api
-        response = requests.get(api_url).json()
-    except:
-        print("Internal API error")
-    return response
+class etlpipe:
 
+    def getApiData(api_url):
+        try:
+            # Extracting the data from api
+            response = requests.get(api_url).json()
+        except:
+            print("Internal API error")
+        return response
 
-def mongodb_conn(hostname, portno):
-    try:
-        # Creating Mongo Client Connection
-        clientCon = MongoClient(hostname, portno)
-        print('****** connection successful ******')
-    except ConnectionError:
-        print("Could not connect to server")
-    return clientCon
+    def mongodb_conn(hostname, portno):
+        try:
+            # Creating Mongo Client Connection
+            clientCon = MongoClient(hostname, portno)
+            print('****** connection successful ******')
+        except ConnectionError:
+            print("Could not connect to server")
+        return clientCon
 
+    def ExtractDB(clientCon, response, DBName, CollName):
+        # Data Base and collection name given otherwise it will create
+        mydb = clientCon[DBName]
+        mycol = mydb[CollName]
+        try:
+            # Creating the index for fast retrival and to avoid inserting duplicates
+            mydb[CollName].create_index([("id", 1)], name="id", unique=True)
+            print('***** Index created *****')
+        except:
+            print('Index already exists')
 
-def ExtractDB(clientCon, response, DBName, CollName):
-    # Data Base and collection name given otherwise it will create
-    mydb = clientCon[DBName]
-    mycol = mydb[CollName]
-    try:
-        # Creating the index for fast retrival and to avoid inserting duplicates
-        mydb[CollName].create_index([("id", 1)], name="id", unique=True)
-        print('***** Index created *****')
-    except:
-        print('Index already exists')
+        for i in response:
+            # Updating the records, if not it will insert the records based on index created id
+            mycol.find_one_and_update(i, {"$set": {"id": i.get("id")}}, upsert=True)
+        return mycol
 
-    for i in response:
-        # Updating the records, if not it will insert the records based on index created id
-        mycol.find_one_and_update(i, {"$set": {"id": i.get("id")}}, upsert=True)
-    return mycol
+    def mongo_export_to_file(mycol, filepath):
 
+        # Getting the data in collection
+        cursor = mycol.find()
 
-def mongo_export_to_file(mycol, filepath):
+        doc_list = []
 
-    # Getting the data in collection
-    cursor = mycol.find()
+        # Adding the documents in list to pass this list to  create dataframe
+        for i in cursor:
+            doc_list.append(i)
 
-    doc_list = []
+        # Convert the doc_list to a DataFrame
+        docs = pandas.DataFrame(doc_list)
 
-    # Adding the documents in list to pass this list to  create dataframe
-    for i in cursor:
-        doc_list.append(i)
+        # export MongoDB documents to a CSV file
+        docs.to_csv(filepath, ",", index=False)  # CSV delimited by commas
 
-    # Convert the doc_list to a DataFrame
-    docs = pandas.DataFrame(doc_list)
-
-    # export MongoDB documents to a CSV file
-    docs.to_csv(filepath, ",", index=False)  # CSV delimited by commas
-
-    print('Data is written to this path  ', filepath)
+        print('Data is written to this path  ', filepath)
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import ETLDemo
+from ETLDemo import etlpipe
 import Configfile
-
 
 def main():
 
     # Calling getApiData and passing the API_URL from configfile
-    responseData = ETLDemo.getApiData(Configfile.API_URL)
+    responseData = etlpipe.getApiData(Configfile.API_URL)
 
     # Instantiating the mongodb_conn function with host name and port number from configfile
-    mongo_client = ETLDemo.mongodb_conn(Configfile.HOST_NAME, Configfile.PORT_No)
+    mongo_client = etlpipe.mongodb_conn(Configfile.HOST_NAME, Configfile.PORT_No)
 
     # Calling the ExtractDB function with mongo_client and response data with Data base and collection name
-    coll = ETLDemo.ExtractDB(mongo_client, responseData, Configfile.DB_NAME, Configfile.COLL_NAME)
+    coll = etlpipe.ExtractDB(mongo_client, responseData, Configfile.DB_NAME, Configfile.COLL_NAME)
 
     # Finally exporting the data to csv file from mongo collection which we inserted data
-    ETLDemo.mongo_export_to_file(coll, Configfile.CSV_PATH)
+    etlpipe.mongo_export_to_file(coll, Configfile.CSV_PATH)
 
 
 if __name__ == '__main__':
